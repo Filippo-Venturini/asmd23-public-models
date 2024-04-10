7411833c4ee4012295f2fc07861378bb1acb4ced
Filippo Venturini <filippo.venturini8@studio.unibo.it>
2024-04-10 16:45:13 +0200

    implemented priorities in task2

--- src/main/scala/u06/modelling/PetriNet.scala
+++ src/main/scala/u06/modelling/PetriNet.scala
@@ -5,8 +5,8 @@ import pc.utils.MSet
 import scala.u06.task1.ReadersWritersPetriNet.Place
 
 object PetriNet:
-  // pre-conditions, effects, inhibition
-  case class Trn[P](cond: MSet[P], eff: MSet[P], inh: MSet[P])
+  // pre-conditions, effects, inhibition, priority
+  case class Trn[P](cond: MSet[P], eff: MSet[P], inh: MSet[P], priority: Int = 1)
   type PetriNet[P] = Set[Trn[P]]
   type Marking[P] = MSet[P]
 
@@ -17,13 +17,16 @@ object PetriNet:
   extension [P](pn: PetriNet[P])
     def toSystem: System[Marking[P]] = m =>
       for
-        Trn(cond, eff, inh) <- pn   // get any transition
+        Trn(cond, eff, inh, priority) <- pn.sortByPriority// get any transition
         if m disjoined inh          // check inhibition
         out <- m extract cond       // remove precondition
       yield out union eff           // add effect
+
+    def sortByPriority: PetriNet[P] = pn.toList.sortBy(-_.priority).toSet
 
   // fancy syntax to create transition rules
   extension [P](self: Marking[P])
     def ~~> (y: Marking[P]) = Trn(self, y, MSet())
   extension [P](self: Trn[P])
     def ^^^ (z: Marking[P]) = self.copy(inh = z)
+    def priority(p: Int) = self.copy(priority = p)
