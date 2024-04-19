380de845e6b17655a542f41e293045f2e2f027d0
Filippo Venturini <filippo.venturini8@studio.unibo.it>
2024-04-19 09:39:57 +0200

    Implemented brussellator oscillation

--- src/main/scala/u07/task3/BrussellatorPN.scala
+++ src/main/scala/u07/task3/BrussellatorPN.scala
@@ -19,20 +19,19 @@ object BrussellatorPN:
   export u07.modelling.SPN.*
 
   val brussellatorPN = SPN[Place](
-    Trn(MSet(), m => 10, MSet(A), MSet()),
-    Trn(MSet(), m => 10, MSet(B), MSet()),
-    Trn(MSet(A), m => m(A) * (1 + 0.1 / math.exp(m(X))), MSet(X), MSet()),
-    Trn(MSet(X, X, Y), m =>  m(X) * m(Y) * (1 + 0.1 / math.exp(m(X))), MSet(X, X, X), MSet()),
-    Trn(MSet(B, X), m => m(B) * m(X) * (1 + 0.1 * math.exp(m(X))), MSet(Y, D), MSet()),
-    Trn(MSet(X), m => m(X) * 1, MSet(E), MSet())
+    Trn(MSet(), m => 1, MSet(A), MSet()),
+    Trn(MSet(), m => 1, MSet(B), MSet()),
+    Trn(MSet(A), m => 1, MSet(X), MSet()),
+    Trn(MSet(X, X, Y), m =>  m(Y), MSet(X, X, X), MSet()),
+    Trn(MSet(B, X), m => m(X) * 0.5, MSet(Y, D), MSet()),
+    Trn(MSet(X), m => m(X) * 0.5, MSet(E), MSet())
   )
 
 @main def mainBrusselatorPN =
 
   val simulation = toCTMC(brussellatorPN).newSimulationTrace(MSet(A,B,B,B,X,Y), new Random)
-    .take(50)
+    .take(500)
     .toList
-  simulation.foreach(println)
 
   val times = simulation.map(_._1).toArray
   val xCounts = simulation.map(_._2.countOccurrences(X)).map(_.toDouble).toArray
@@ -40,6 +39,8 @@ object BrussellatorPN:
 
   val chart = QuickChart.getChart("Brusselator Simulation", "Time", "Count", "X", times, xCounts)
   chart.addSeries("Y", times, yCounts)
+
   chart.getStyler.setLegendVisible(true)
+  chart.getStyler.setMarkerSize(0)
 
   new SwingWrapper[XYChart](chart).displayChart().setTitle("Brusselator Simulation")
